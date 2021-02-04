82b94bf23f8843623618d4ff7f7593e46796f16f
ankit barfa <pujasolanki0599@gmail.com>
2021-02-04 23:51:25 +0530

    changes

--- src/main/scala/com/test/Gittest.java
+++ src/main/scala/com/test/Gittest.java
@@ -3,5 +3,6 @@ package com.test;
 public class Gittest {
     public static void main(String[] args) {
         System.out.println("hello ankit");
+        System.out.println("hello puji");
     }
 }
